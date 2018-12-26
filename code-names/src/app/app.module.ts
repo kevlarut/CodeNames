f1297bfe6c03e0aaea1c042a157a3a915f6d191a
Jeremy <jeremyneilson31@gmail.com>
2018-12-26 16:47:23 -0700

    latest code name changes

--- code-names/src/app/app.module.ts
+++ code-names/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { GameComponent } from './game/game.component';
@@ -10,7 +11,8 @@ import { GameComponent } from './game/game.component';
     GameComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ code-names/src/app/tile.model.ts
@@ -0,0 +1,9 @@
+export class Tile {
+    public word: string;
+    public allegiance: string;
+
+    constructor(word: string, allegiance: string) {
+        this.word = word;
+        this.allegiance = allegiance;
+    }
+}
